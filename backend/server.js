65d039baae78b092af96a1464e64b2caffa0029f
Khushboo Kumari <khushbookumari200314@gmail.com>
2026-03-06 20:44:44 +0530

    Move CORS origin to env variable

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const app = express()
 connectDB()
 
 // Middleware
-app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}))
 app.use(helmet())
 app.use(express.json())
 
